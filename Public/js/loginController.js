3268e51d223e06f8bd1f786055c5ed722f36bb4a
priyankashaji <priyankashaji@gmail.com>
2017-03-15 11:22:35 -0400

    updates in the vedio

--- Public/js/loginController.js
+++ Public/js/loginController.js
@@ -2,4 +2,5 @@ var app = angular.module("medMod");
 
 app.controller("loginController", function($scope){
   $scope.practice = "loginpage";
+  document.getElementById("my-video").playbackRate = 0.5;
 });
